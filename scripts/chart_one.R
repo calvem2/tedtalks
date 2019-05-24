d2d94b553378b0b2dd10716103e32091915acce4
unknown <trosts@uw.edu>
2019-05-24 13:51:39 -0700

    add chart one description

--- scripts/chart_one.R
+++ scripts/chart_one.R
@@ -8,11 +8,6 @@ library("ggplot2")
 library("plotly")
 library("scales")
 
-
-# Take in data
-ted_talks_main <- read.csv("data/ted_main.csv", stringsAsFactors = TRUE)
-
-
 # Purpose of visualization: The reason for using a scatter plot for this
 # visualization is to determine if there is a relationship between the length
 # of the video and the number of views it recieves. 
@@ -20,21 +15,23 @@ ted_talks_main <- read.csv("data/ted_main.csv", stringsAsFactors = TRUE)
 
 # Wrangle data
 
-categories_of_interest <- ted_talks_main %>% 
+categories_of_interest <- function(ted_main){
+  ted_main <- ted_main %>%
   select(views, duration) %>% 
-  mutate(duration_minutes = duration / 60) %>% 
-  round(1)
+    mutate(duration_minutes = duration / 60) %>% 
+    round(1)
+}
+
 
 # Plot data
 
- plot_viewership_duration <- function(ted_talks_main){
-   ggplot(categories_of_interest,aes(x= duration_minutes,y= views)) + geom_point(alpha = 0.2)+
+ plot_viewership_duration <- function(ted_main){
+   ggplot(categories_of_interest(ted_main),aes(x = duration_minutes,y = views)) + geom_point(alpha = 0.2)+
      geom_smooth(mapping = aes(x = duration_minutes, y = views))+
      xlab("Duration in Minutes") + 
      ylab("Number of Views")+
      labs(title = "Duration of Ted Talk vs. Viewership")
-   }
-
+ }
 
 
 
