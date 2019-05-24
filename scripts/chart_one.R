5e1c1138227f753b969ead2079375a67e103085d
unknown <trosts@uw.edu>
2019-05-24 15:03:20 -0700

    lint chart_one

--- scripts/chart_one.R
+++ scripts/chart_one.R
@@ -7,6 +7,7 @@ library("tidyr")
 library("ggplot2")
 library("plotly")
 library("scales")
+library("lintr")
 
 # Purpose of visualization: The reason for using a scatter plot for this
 # visualization is to determine if there is a relationship between the length
@@ -17,8 +18,8 @@ library("scales")
 
 categories_of_interest <- function(ted_main){
   ted_main <- ted_main %>%
-  select(views, duration) %>% 
-    mutate(duration_minutes = duration / 60) %>% 
+  select(views, duration) %>%
+    mutate(duration_minutes = duration / 60) %>%
     round(1)
 }
 
@@ -26,13 +27,10 @@ categories_of_interest <- function(ted_main){
 # Plot data
 
  plot_viewership_duration <- function(ted_main){
-   ggplot(categories_of_interest(ted_main),aes(x = duration_minutes,y = views)) + geom_point(alpha = 0.2)+
-     geom_smooth(mapping = aes(x = duration_minutes, y = views))+
-     xlab("Duration in Minutes") + 
-     ylab("Number of Views")+
+ggplot(categories_of_interest(ted_main), aes(x = duration_minutes, y = views)) +
+    geom_point(alpha = 0.2) +
+     geom_smooth(mapping = aes(x = duration_minutes, y = views)) +
+     xlab("Duration in Minutes") +
+     ylab("Number of Views") +
      labs(title = "Duration of Ted Talk vs. Viewership")
- }
-
-
-
-
+}
