b61c84bfa6e1d762cdc4e62511696aeaa4512cd3
Megan Calverley <calvem2@uw.edu>
2019-05-24 15:01:49 -0700

    Lint file

--- scripts/table.R
+++ scripts/table.R
@@ -12,18 +12,21 @@ library("tidyr")
 create_table <- function(dataframe) {
   # Add markdown to insert a link to the Talk within the title column
   dataframe$title <- paste0("[", dataframe$title, "](", dataframe$url, ")")
-  
+
   # Convert the film dates in the data frame
   dataframe$film_date <- anydate(dataframe$film_date)
-  
-  
-  sum_table <- dataframe %>% arrange(desc(views)) %>%
-    mutate(index = row_number() -1) %>%
-    filter(index < 10) %>% 
-    mutate(tags = gsub("\\[|\'|\\]", "", tags) )%>%
-    select(film_date ,title, main_speaker, tags, languages, comments, 
-           views)
-  
+
+
+  sum_table <- dataframe %>%
+    arrange(desc(views)) %>%
+    mutate(index = row_number() - 1) %>%
+    filter(index < 10) %>%
+    mutate(tags = gsub("\\[|\'|\\]", "", tags)) %>%
+    select(
+      film_date, title, main_speaker, tags, languages, comments,
+      views
+    )
+
   return(sum_table)
 }
 
